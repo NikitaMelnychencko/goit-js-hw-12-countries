2f282927e06974e8e87cb931342763f104260e8e
NikitaMelnychencko <hunter6059@gmail.com>
2021-10-13 16:04:47 +0300

    Add feature

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,13 +1,51 @@
+import {notice, defaultModules} from '@pnotify/core';
+import * as PNotifyDesktop from '@pnotify/desktop';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+
+
 export default function fetchCountries(searchQuery) {
- const r = fetch(`https://restcountries.com/v2/name/{searchQuery}`)
-  .then(response => {
-    return response.json()
-  })
-  .then(country => {
-    console.log(country);
-  }
+ return fetch(`https://restcountries.com/v2/name/${searchQuery}`)
+   .then(response => {
+      console.log(response);
+      return response.json();
+    })
+   .catch(error => {
+       const myError = notice({
+          text: "Error 404.",
+          modules: new Map([
+            ...defaultModules,
+            [PNotifyDesktop, {
+            }]
+          ])
+       });
 
-  ) 
+    });
 }
-
-
+// function renderCountry(country) {
+//   if (country.length >= 2 && country.length <= 10) {
+//         const markupList = listCountry(country);
+//         refsRender.innerHTML = markupList;
+//         const refsListCountry = document.querySelector('.country-list');
+//         refsListCountry.addEventListener('click', targetValue);
+//       } else if ((country.length === 1)) {
+//         const markupCard = cardCountry(country);
+//         refsRender.innerHTML = markupCard;
+//       } else {
+//         const myNotice = notice({
+//           text: "Too many matches found. Please enter a mare specific query!",
+//           modules: new Map([
+//             ...defaultModules,
+//             [PNotifyDesktop, {
+//             }]
+//           ])
+//         });
+//       }
+// }
+// function targetValue(e) {
+//   if (e.target.nodeName !== 'LI') {
+//     return;
+//   }
+//   refsInput.value = e.target.textContent;
+//   fetchCountries(refsInput.value)
+// }
--- src/js/modual-1.js
+++ src/js/modual-1.js
@@ -1,12 +1,46 @@
 import fetchCountries from './fetchCountries'
+import listCountry from '../layouts/list-country.hbs';
+import cardCountry from '../layouts/card-country.hbs';
+import {notice, defaultModules} from '@pnotify/core';
+import * as PNotifyDesktop from '@pnotify/desktop';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
 const debounce = require('lodash.debounce');
 
 
 const refsInput = document.querySelector('input')
+const refsRender = document.querySelector('.render');
 
-refsInput.addEventListener('input', e => {
+refsInput.addEventListener('input', debounce(e => {
   e.preventDefault();
-  const value = e.currentTarget.value
-  const result = fetchCountries(value)
+  if (e.target.value.length > 0) {
+    fetchCountries(e.target.value).then(renderCountry).catch(e=>console.log(e))
+  }
+},500))
+function renderCountry(country) {
+  if (country.length >= 2 && country.length <= 10) {
+        const markupList = listCountry(country);
+        refsRender.innerHTML = markupList;
+        const refsListCountry = document.querySelector('.country-list');
+        refsListCountry.addEventListener('click', targetValue);
+      } else if ((country.length === 1)) {
+        const markupCard = cardCountry(country);
+        refsRender.innerHTML = markupCard;
+  }else {
+        const myNotice = notice({
+          text: "Too many matches found. Please enter a mare specific query!",
+          modules: new Map([
+            ...defaultModules,
+            [PNotifyDesktop, {
+            }]
+          ])
+        });
+      }
 }
-)
+function targetValue(e) {
+  if (e.target.nodeName !== 'LI') {
+    return;
+  }
+  refsInput.value = e.target.textContent;
+  fetchCountries(refsInput.value).then(renderCountry)
+}
